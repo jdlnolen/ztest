ac5bc1fddc45dd7ae2301713c61c9fe57c35a9ee
vagrant <vagrant@precise64.(none)>
2014-08-16 21:09:28 +0000

    dumped html for codeship to see

--- tests/functional/first.js
+++ tests/functional/first.js
@@ -18,6 +18,7 @@ describe('home page', function() {
    });
 
    it('should show a greeting', function() {
+       console.log(this.browser.html());
        assert.equal(this.browser.text('#greet'), 'Hello World!');
    });
 });
